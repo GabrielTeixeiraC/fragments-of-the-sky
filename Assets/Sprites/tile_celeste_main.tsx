35606ddd8101b5f3471fa3585be516f460d33631
Gabriel <gteixeiraca@gmail.com>
2025-06-30 18:17:00 -0300

    Map draft

---
+++ Assets/Sprites/tile_celeste_main.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tile_celeste_main" tilewidth="32" tileheight="32" tilecount="1440" columns="30">
+ <image source="celeste_sprites/tile_celeste_main.png" width="960" height="1536"/>
+</tileset>
