66b8b194a2e6eb457e54a18edf2dbe5493282b6b
Gabriel <gteixeiraca@gmail.com>
2025-07-03 04:22:35 -0300

    Convert objects structure to Tile_*

--- Assets/Levels/Level1/objects.tsx
+++ Assets/Levels/Level1/objects.tsx
@@ -2,180 +2,180 @@
 <tileset version="1.10" tiledversion="1.11.2" name="Objects" tilewidth="199" tileheight="183" tilecount="59" columns="0">
     <grid orientation="orthogonal" width="1" height="1"/>
     <tile id="0">
-    <image source="../../Sprites/Swamp/Objects/Boxes/1.png" width="34" height="28"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_01.png" width="34" height="28"/>
     </tile>
     <tile id="1">
-        <image source="../../Sprites/Swamp/Objects/Boxes/2.png" width="34" height="27"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_02.png" width="34" height="27"/>
     </tile>
     <tile id="2">
-        <image source="../../Sprites/Swamp/Objects/Boxes/3.png" width="27" height="26"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_03.png" width="27" height="26"/>
     </tile>
     <tile id="3">
-        <image source="../../Sprites/Swamp/Objects/Boxes/4.png" width="33" height="26"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_04.png" width="33" height="26"/>
     </tile>
     <tile id="4">
-        <image source="../../Sprites/Swamp/Objects/Boxes/5.png" width="34" height="27"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_05.png" width="34" height="27"/>
     </tile>
     <tile id="5">
-        <image source="../../Sprites/Swamp/Objects/Boxes/6.png" width="27" height="26"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_06.png" width="27" height="26"/>
     </tile>
     <tile id="6">
-        <image source="../../Sprites/Swamp/Objects/Bushes/1.png" width="31" height="15"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_07.png" width="31" height="15"/>
     </tile>
     <tile id="7">
-        <image source="../../Sprites/Swamp/Objects/Bushes/2.png" width="30" height="10"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_08.png" width="30" height="10"/>
     </tile>
     <tile id="8">
-        <image source="../../Sprites/Swamp/Objects/Bushes/3.png" width="29" height="15"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_09.png" width="29" height="15"/>
     </tile>
     <tile id="9">
-        <image source="../../Sprites/Swamp/Objects/Bushes/4.png" width="31" height="21"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_10.png" width="31" height="21"/>
     </tile>
     <tile id="10">
-        <image source="../../Sprites/Swamp/Objects/Bushes/5.png" width="63" height="28"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_11.png" width="63" height="28"/>
     </tile>
     <tile id="11">
-        <image source="../../Sprites/Swamp/Objects/Bushes/6.png" width="31" height="26"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_12.png" width="31" height="26"/>
     </tile>
     <tile id="12">
-        <image source="../../Sprites/Swamp/Objects/Bushes/7.png" width="61" height="24"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_13.png" width="61" height="24"/>
     </tile>
     <tile id="13">
-        <image source="../../Sprites/Swamp/Objects/Bushes/8.png" width="63" height="27"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_14.png" width="63" height="27"/>
     </tile>
     <tile id="14">
-        <image source="../../Sprites/Swamp/Objects/Bushes/9.png" width="61" height="25"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_15.png" width="61" height="25"/>
     </tile>
     <tile id="15">
-        <image source="../../Sprites/Swamp/Objects/Fence/1.png" width="23" height="18"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_16.png" width="23" height="18"/>
     </tile>
     <tile id="16">
-        <image source="../../Sprites/Swamp/Objects/Fence/2.png" width="25" height="20"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_17.png" width="25" height="20"/>
     </tile>
     <tile id="17">
-        <image source="../../Sprites/Swamp/Objects/Fence/3.png" width="16" height="13"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_18.png" width="16" height="13"/>
     </tile>
     <tile id="18">
-        <image source="../../Sprites/Swamp/Objects/Grass/1.png" width="17" height="18"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_19.png" width="17" height="18"/>
     </tile>
     <tile id="19">
-        <image source="../../Sprites/Swamp/Objects/Grass/2.png" width="17" height="16"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_20.png" width="17" height="16"/>
     </tile>
     <tile id="20">
-        <image source="../../Sprites/Swamp/Objects/Grass/3.png" width="14" height="16"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_21.png" width="14" height="16"/>
     </tile>
     <tile id="21">
-        <image source="../../Sprites/Swamp/Objects/Grass/4.png" width="12" height="12"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_22.png" width="12" height="12"/>
     </tile>
     <tile id="22">
-        <image source="../../Sprites/Swamp/Objects/Grass/5.png" width="11" height="13"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_23.png" width="11" height="13"/>
     </tile>
     <tile id="23">
-        <image source="../../Sprites/Swamp/Objects/Grass/6.png" width="11" height="9"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_24.png" width="11" height="9"/>
     </tile>
     <tile id="24">
-        <image source="../../Sprites/Swamp/Objects/Grass/7.png" width="7" height="14"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_25.png" width="7" height="14"/>
     </tile>
     <tile id="25">
-        <image source="../../Sprites/Swamp/Objects/Grass/8.png" width="8" height="14"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_26.png" width="8" height="14"/>
     </tile>
     <tile id="26">
-        <image source="../../Sprites/Swamp/Objects/Grass/9.png" width="6" height="8"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_27.png" width="6" height="8"/>
     </tile>
     <tile id="27">
-        <image source="../../Sprites/Swamp/Objects/Grass/10.png" width="9" height="11"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_28.png" width="9" height="11"/>
     </tile>
     <tile id="28">
-        <image source="../../Sprites/Swamp/Objects/Ladders/1.png" width="32" height="32"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_29.png" width="32" height="32"/>
     </tile>
     <tile id="29">
-        <image source="../../Sprites/Swamp/Objects/Ladders/2.png" width="32" height="32"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_30.png" width="32" height="32"/>
     </tile>
     <tile id="30">
-        <image source="../../Sprites/Swamp/Objects/Ladders/3.png" width="32" height="32"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_31.png" width="32" height="32"/>
     </tile>
     <tile id="31">
-        <image source="../../Sprites/Swamp/Objects/Ladders/4.png" width="32" height="32"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_32.png" width="32" height="32"/>
     </tile>
     <tile id="32">
-        <image source="../../Sprites/Swamp/Objects/Ladders/5.png" width="32" height="32"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_33.png" width="32" height="32"/>
     </tile>
     <tile id="33">
-        <image source="../../Sprites/Swamp/Objects/Ladders/6.png" width="32" height="32"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_34.png" width="32" height="32"/>
     </tile>
     <tile id="34">
-        <image source="../../Sprites/Swamp/Objects/Pointers/1.png" width="16" height="21"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_35.png" width="16" height="21"/>
     </tile>
     <tile id="35">
-        <image source="../../Sprites/Swamp/Objects/Pointers/2.png" width="16" height="21"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_36.png" width="16" height="21"/>
     </tile>
     <tile id="36">
-        <image source="../../Sprites/Swamp/Objects/Pointers/3.png" width="13" height="21"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_37.png" width="13" height="21"/>
     </tile>
     <tile id="37">
-        <image source="../../Sprites/Swamp/Objects/Pointers/4.png" width="13" height="21"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_38.png" width="13" height="21"/>
     </tile>
     <tile id="38">
-        <image source="../../Sprites/Swamp/Objects/Pointers/5.png" width="13" height="21"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_39.png" width="13" height="21"/>
     </tile>
     <tile id="39">
-        <image source="../../Sprites/Swamp/Objects/Pointers/6.png" width="13" height="21"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_40.png" width="13" height="21"/>
     </tile>
     <tile id="40">
-        <image source="../../Sprites/Swamp/Objects/Pointers/7.png" width="13" height="21"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_41.png" width="13" height="21"/>
     </tile>
     <tile id="41">
-        <image source="../../Sprites/Swamp/Objects/Pointers/8.png" width="11" height="21"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_42.png" width="11" height="21"/>
     </tile>
     <tile id="42">
-        <image source="../../Sprites/Swamp/Objects/Ridges/1.png" width="89" height="26"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_42.png" width="89" height="26"/>
     </tile>
     <tile id="43">
-        <image source="../../Sprites/Swamp/Objects/Ridges/2.png" width="40" height="28"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_43.png" width="40" height="28"/>
     </tile>
     <tile id="44">
-        <image source="../../Sprites/Swamp/Objects/Ridges/3.png" width="109" height="41"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_44.png" width="109" height="41"/>
     </tile>
     <tile id="45">
-        <image source="../../Sprites/Swamp/Objects/Ridges/4.png" width="65" height="61"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_45.png" width="65" height="61"/>
     </tile>
     <tile id="46">
-        <image source="../../Sprites/Swamp/Objects/Ridges/5.png" width="81" height="29"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_46.png" width="81" height="29"/>
     </tile>
     <tile id="47">
-        <image source="../../Sprites/Swamp/Objects/Ridges/6.png" width="47" height="55"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_47.png" width="47" height="55"/>
     </tile>
     <tile id="48">
-        <image source="../../Sprites/Swamp/Objects/Stones/1.png" width="63" height="59"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_48.png" width="63" height="59"/>
     </tile>
     <tile id="49">
-        <image source="../../Sprites/Swamp/Objects/Stones/2.png" width="47" height="57"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_49.png" width="47" height="57"/>
     </tile>
     <tile id="50">
-        <image source="../../Sprites/Swamp/Objects/Stones/3.png" width="52" height="63"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_50.png" width="52" height="63"/>
     </tile>
     <tile id="51">
-        <image source="../../Sprites/Swamp/Objects/Stones/4.png" width="57" height="30"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_51.png" width="57" height="30"/>
     </tile>
     <tile id="52">
-        <image source="../../Sprites/Swamp/Objects/Stones/5.png" width="31" height="31"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_52.png" width="31" height="31"/>
     </tile>
     <tile id="53">
-        <image source="../../Sprites/Swamp/Objects/Trees/1.png" width="101" height="103"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_53.png" width="101" height="103"/>
     </tile>
     <tile id="54">
-        <image source="../../Sprites/Swamp/Objects/Trees/2.png" width="104" height="132"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_54.png" width="104" height="132"/>
     </tile>
     <tile id="55">
-        <image source="../../Sprites/Swamp/Objects/Trees/3.png" width="123" height="150"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_55.png" width="123" height="150"/>
     </tile>
     <tile id="56">
-        <image source="../../Sprites/Swamp/Objects/Willows/1.png" width="104" height="135"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_56.png" width="104" height="135"/>
     </tile>
     <tile id="57">
-        <image source="../../Sprites/Swamp/Objects/Willows/2.png" width="141" height="168"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_57.png" width="141" height="168"/>
     </tile>
     <tile id="58">
-        <image source="../../Sprites/Swamp/Objects/Willows/3.png" width="199" height="183"/>
+        <image source="../../Sprites/Swamp/Objects/Tile_58.png" width="199" height="183"/>
     </tile>
 </tileset>
